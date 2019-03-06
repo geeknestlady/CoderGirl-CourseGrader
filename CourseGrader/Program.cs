c4a0b358b630713a0052388b5645d27c23b8984e
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-05 21:34:07 -0600

    almost there

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -6,7 +6,7 @@ namespace CourseGrader
     {
         public static void Main(string[] args)
         {
-            int[] testScores = new int[5] { 46, 75, 83, 89, 91 };
+            int[] testScores = new int[5] { 50, 75, 83, 89, 91 };
 
            string passFail = GradeTestScores(testScores);
             Console.WriteLine(passFail);
@@ -17,39 +17,34 @@ namespace CourseGrader
             // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
             // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
         }
-        public static string GradeTestScores(int [] testScores)
+        public static string GradeTestScores(int[] testScores)
         {
             string pass;
-                              
             int sum = 0;
-            
+            if (testScores == null || testScores.Length == 0)
+                return "fail";
             foreach (int score in testScores)
             {
-                sum += score;
-                                              
+                sum += score;                                              
             } 
             
             int average = sum / testScores.Length;
             if (average >= 70)
             {
-                pass = "pass";
-                
+                pass = "pass";                
             }
-            else if (average >= 70)
+            else if (average < 70)
             {
                 pass = "fail";
-                
             }
             else
             { 
-                pass = "fail";
-                
+                pass = "fail";                
             }
             for (int i = 0; i < testScores.Length; i++)
             {
-                if (testScores[i] == 0 && testScores[i] <= 50)
-                    pass = "fail";
-                return pass;
+                if (testScores == null && testScores[i] <= 50)
+                    pass = "fail";                           
             }
             return pass;
         }
